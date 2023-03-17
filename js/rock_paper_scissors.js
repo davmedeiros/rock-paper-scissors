a8ddd848c7b892d86d9befe211f829a767776fdc
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-17 14:58:46 +0000

    Add getComputerChoice()

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -1,1 +1,8 @@
-console.log("rocking");
+console.log(getComputerChoice());
+
+// Randomly picks either Rock, Paper or Scissors
+function getComputerChoice() {
+  choices = ['rock', 'paper', 'scissors'];
+
+  return choices[Math.floor(Math.random() * 3)];
+}
