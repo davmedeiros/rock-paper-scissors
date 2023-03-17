f93e6896ab488995e58a9157fdd9d5b63f40c4d5
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-17 15:21:31 +0000

    Add playRound() with pseudocode

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -6,3 +6,27 @@ function getComputerChoice() {
 
   return choices[Math.floor(Math.random() * 3)];
 }
+
+// Plays a round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // If choices are the same
+    // Issa tie
+
+  // If pS = rock and cS = paper
+    // Player loses
+  // If pS = rock and cS = scissors
+    // Player wins
+
+  // If pS = paper and cS = rock
+    // Player wins
+  // If pS = paper and cS = scissors
+    // Player loses
+  
+  // If pS = scissors and cS = rock
+    // Player loses
+  // If pS = scissors and cS = paper
+    // Player wins
+
+  // Return round result
+  return 'Max bidding. Better believe it.';
+}
