b59c58445e5fcb2413e8015991f5592f149bc092
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-07-21 17:36:23 +0100

    Create playRound with game logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
+function getAvailableChoices() {
+    return ['rock', 'paper', 'scissors'];
+}
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = getAvailableChoices();
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result = '';
+
+    if (playerSelection == computerSelection) {
+        result = 'It\'s a tie!';
+    } else {
+        if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+            playerSelection == 'paper' && computerSelection == 'rock' ||
+            playerSelection == 'scissors' && computerSelection == 'paper') {
+            result = `You Win! ${playerSelection} beats ${computerSelection}.`;
+        } else {
+            result = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        }
+    }
+
+    return result;
 }
